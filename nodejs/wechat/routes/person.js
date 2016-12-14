a9753418e35c08e655f69e6f3855604fe2b9511b
yql <445375039@qq.com>
2016-12-14 18:24:48 +0800

    提交

--- nodejs/wechat/routes/person.js
+++ nodejs/wechat/routes/person.js
@@ -2,9 +2,63 @@ var express = require('express');
 var router = express.Router();
 var httpUtil = require('../lib/util/httpUtil');
 
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
 	console.log("session中的userId:"+req.session.userId);
 	res.render('person', { msg: "进入个人信息界面" });
+});*/
+
+var queryUser = function(req, res){
+    var validCode = req.query.validCode;
+    var data = {
+        userId: req.session.userId
+    };
+    var url = "/e/queryUser.do";
+    httpUtil.post("127.0.0.1", "8080", url, JSON.stringify(data)).then(function(data){
+        // data = JSON.parse(data);
+        res.send(data);
+        /*if(data && data.status && data.content){
+            res.send(data);
+        }else{
+            res.send({status:false, msg: data.msg});
+        }*/
+    }).catch(function (err) {
+        console.log('获取用户个人信息失败:'+err);
+        res.send({status:false, msg:"获取用户个人信息失败"});
+    });
+};
+
+var saveUser = function(req, res){
+    var data = req.query.data;
+    data.userId = req.session.userId;
+    var url = "/e/saveUser.do";
+    httpUtil.post("127.0.0.1", "8080", url, JSON.stringify(data)).then(function(data){
+        // data = JSON.parse(data);
+        res.send(data);
+        // if(data && data.status && data.content){
+        //     res.send(data);
+        // }else{
+        //     res.send({status:false, msg: data.msg});
+        // }
+    }).catch(function (err) {
+        console.log('保存失败:'+err);
+        res.send({status:false, msg:"保存失败"});
+    });
+};
+
+router.get('/:id', function(req, res, next) {
+    console.log("session中的userId:"+req.session.userId);
+    console.log("session中的openid:"+req.session.openid);
+    var id = req.params.id;
+    console.log("id:"+id);
+    if(id == "index"){
+        res.render('person', { msg: "进入个人信息界面" });
+    }else if(id == "queryUser"){
+        queryUser(req, res);
+    }else if(id == "editPersonNickName"){
+        res.render('editPersonNickName', { msg: "修改昵称" });
+    }else if(id == "saveUser"){
+        saveUser(req, res);
+    }
 });
 
 module.exports = router;
