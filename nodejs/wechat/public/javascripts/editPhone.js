0751c3b787df2dabd7d5316e275a9c1f245ad6c4
yqil <445375039#qq.com>
2016-12-14 23:52:31 +0800

    2016年12月14日

--- nodejs/wechat/public/javascripts/editPhone.js
+++ nodejs/wechat/public/javascripts/editPhone.js
@@ -3,18 +3,18 @@
     var vm = new Vue({
         el: "#wxEditMmh",
         data: {
-            mmh: null,
+            phone: null,
             dialogMsg: "服务器开小差了，请稍后尝试~"
         },
         mounted: function(){
-            this.mmh = this.getQueryString("mmh");
+            this.phone = this.getQueryString("phone");
         },
         methods: {
 
             save: function(){
                 var data = {
                     data: {
-                        mmh: this.mmh
+                        phone: this.phone
                     }
                 };
                 $.getJSON('/person/saveUser', data, function(data, status, xhr){
