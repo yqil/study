8ac6c7df57f70e31aca79d5462a4a71b0348175f
yql <yql@localhost.localdomain>
2016-12-15 02:07:35 -0800

    commit

---
+++ nodejs/wechat/public/javascripts/editAddr.js
@@ -0,0 +1,31 @@
+
+! function() {
+    var vm = new Vue({
+        el: "#wxEditAddr",
+        data: {
+            mmh: null,
+            dialogMsg: "服务器开小差了，请稍后尝试~",
+            loaddingMsg: "保存中，请稍后~"
+        }
+        methods: {
+            save: function(){
+                var that = this;
+                var data = {
+                    data: {
+                        mmh: this.mmh
+                    }
+                };
+                this.myLoadding.showLoadding();
+                $.getJSON('/person/saveUser', data, function(data, status, xhr){
+                    if(data){
+                        that.myLoadding.closeLoadding();
+                        window.location.href="/person/index";
+                    }else {
+                        that.myLoadding.closeLoadding();
+                        that.myDialog.showDialog();
+                    }
+                });
+            }
+        }
+    });
+}();
