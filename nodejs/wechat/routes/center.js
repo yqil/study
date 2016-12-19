cacd057820585d28ae00a6837722333a9acda651
yql <yql@localhost.localdomain>
2016-12-19 01:55:00 -0800

    commit

--- nodejs/wechat/routes/center.js
+++ nodejs/wechat/routes/center.js
@@ -2,30 +2,73 @@ var express = require('express');
 var router = express.Router();
 var httpUtil = require('../lib/util/httpUtil');
 
+var queryUserInfo = function(userId){
+	return new Promise(function(resolver, reject) {
+		var data = {
+			userId: userId
+		};
+		var url = "/e/queryUser.do?userId="+userId;
+		httpUtil.get("127.0.0.1", "8080", url).then(function(data){
+			data = JSON.parse(data);
+			if(data && data.status && data.content){
+				resolver(data);
+			}else {
+				resolver({status:false, msg:"获取用户信息失败"});
+			}
+		}).catch(function (err) {
+	  		console.log('获取用户信息失败:'+err);
+	  		reject(err);
+		});
+	});
+};
+
+var queryUserAccount = function(userId){
+	return new Promise(function(resolver, reject) {
+		var url = "/e/queryAccount.do?type=JIFEN&userId="+userId
+		httpUtil.get("127.0.0.1", "8080", url).then(function(data){
+			data = JSON.parse(data);
+			if(data && data.status && data.content){
+				resolver(data);
+			}else {
+				resolver({status:false, msg:"获取用户积分信息失败"});
+			}
+		}).catch(function (err) {
+	  		console.log('获取用户积分信息失败:'+err);
+	  		reject(err);
+		});
+	});
+};
+
 router.get('/', function(req, res, next) {
 	console.log("session中的userId:"+req.session.userId);
 	var userId = req.session.userId;
 	if(userId){
-		var data = {
-			userId: userId
-		};
-		var url = "/e/queryUser.do";
-		httpUtil.post("127.0.0.1", "8080", url, JSON.stringify(data)).then(function(data){
-			data = JSON.parse(data);
-			if(data && data.status && data.content){
-				res.send(data);
+		queryUserInfo(userId).then(function(userInfo){
+			if(userInfo && userInfo.status){
+				queryUserAccount(userId).then(function(accountInfo){
+					if(accountInfo && accountInfo.status){
+						res.send({
+							userInfo: userInfo.content,
+							accountInfo: accountInfo.content
+						});
+					}else {
+						res.send(accountInfo);
+					}
+				}).catch(function(err){
+					console.log('获取用户积分信息失败:'+err);
+			  		res.send({status:false, msg:"获取用户积分信息失败"});
+				});
 			}else {
-				res.render('index', { msg: "进入用户绑定界面" });
+				res.send(userInfo);
 			}
-		}).catch(function (err) {
-	  		console.log('获取用户信息失败:'+err);
+		}).catch(function(err){
+			console.log('获取用户信息失败:'+err);
 	  		res.send({status:false, msg:"获取用户信息失败"});
 		});
 	}else {
 		console.log('session中的userId不存在');
 	  		res.send({status:false, msg:"系统出现异常"});
-	}
-	
+	}	
 });
 
 module.exports = router;
