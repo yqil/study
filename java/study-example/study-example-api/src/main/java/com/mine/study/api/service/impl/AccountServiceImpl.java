044af692857bd35e34469aa749ee4bd04204acc4
yql <445375039@qq.com>
2016-12-19 16:17:34 +0800

    commit

---
+++ java/study-example/study-example-api/src/main/java/com/mine/study/api/service/impl/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.mine.study.api.service.impl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mine.study.api.dao.autocode.TUserAccountMapper;
+import com.mine.study.common.model.autocode.TUserAccount;
+import com.mine.study.common.model.autocode.TUserAccountExample;
+import com.mine.study.common.service.AccountService;
+import com.mine.study.whole.util.collections.ListUtils;
+import com.mine.study.whole.util.exception.SysException;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService
+{
+    private static Logger log = LoggerFactory.getLogger(AccountServiceImpl.class);
+    
+    @Autowired
+    private TUserAccountMapper userAccountMapper;
+
+    @Override
+    public TUserAccount queryAccount(String userId, String type) throws SysException
+    {
+        log.info("queryAccount传入参数:{}", userId);
+        TUserAccountExample example = new TUserAccountExample();
+        example.createCriteria().andUserIdEqualTo(userId).andAccountTypeEqualTo(type);
+        List<TUserAccount> rs = userAccountMapper.selectByExample(example);
+        if(ListUtils.isEmpty(rs)){
+            return null;
+        }else {
+            return rs.get(0);
+        }
+    }
+
+}
