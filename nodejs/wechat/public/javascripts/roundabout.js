d0e1066a27bf7f0adbe659fa6e044d91578d1ce0
yql <yql@localhost.localdomain>
2016-12-21 22:41:41 -0800

    commit

--- nodejs/wechat/public/javascripts/roundabout.js
+++ nodejs/wechat/public/javascripts/roundabout.js
@@ -2,28 +2,74 @@
     var vm = new Vue({
         el: "#wxRoundabout",
         data: {
-            integral: 888,
+            freeNum: 8,
+            prizeList: [
+                {phone: "666****6666", prizeName:"四件套1"},
+                {phone: "666****6666", prizeName:"四件套2"},
+                {phone: "666****6666", prizeName:"四件套3"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"},
+                {phone: "666****6666", prizeName:"四件套床品套件"}
+                ],
+            timer: null,
+            firstInitTop: 0,
+            secondInitTop: 0,
+            maxInitTop: 0,
             dialogMsg: "服务器开小差了，请稍后尝试~"
         },
+        mounted: function(){
+            this.initPrizeRollList();
+        },
         methods: {
-            accessLottery: function(e){
-                var type = $(e.target).data("type");
-                var status = $(e.target).data("status");
-                if(status == "1"){
-                    if(type == "1"){
-                        window.location.href = "/lottery/roundabout";
-                    }else if(type == "2"){
-                        window.location.href = "/lottery/scratchCard";
-                    }else if(type == "3"){
-                        window.location.href = "/lottery/goldEgg";
-                    }
-                }else if(status == "2"){
-                    this.dialogMsg = "活动还未开始，敬请关注~";
-                    this.myDialog.showDialog();
-                }else if(status == "3"){
-                    this.dialogMsg = "活动已结束，请关注其他活动~";
-                    this.myDialog.showDialog();
+            initPrizeRollList: function(){
+                var prizeListSize = 0;
+                var strHtml = "";
+                var len = this.prizeList.length;
+                if(len % 2 == 1){
+                    prizeListSize = Math.floor(len/2) +1;
+                }else {
+                    prizeListSize = Math.floor(len/2);
                 }
+                for(var i = 0; i < len; i += 2){
+                    strHtml += '<ul class="row wx-clear-margin wx-roundabout-rollList-row">'+
+                        '<li class="col-xs-3 text-center wx-clear-padding">'+this.prizeList[i].phone+'</li>'+
+                        '<li class="col-xs-3 text-center wx-clear-padding">'+this.prizeList[i].prizeName+'</li>';
+                    if(i < len - 1){
+                        strHtml += '<li class="col-xs-3 text-center wx-clear-padding">'+this.prizeList[i+1].phone+'</li>'+
+                            '<li class="col-xs-3 text-center wx-clear-padding">'+this.prizeList[i+1].prizeName+'</li>';
+                    }
+                    strHtml += '</ul>';
+                }
+
+                this.maxTop = 30 * prizeListSize;
+                $("#firstRollList").html(strHtml);
+                this.firstTop = 0;
+                $("#firstRollList").css("top", "0");
+                if(prizeListSize > 4){
+                    $("#secondRollList").html(strHtml);
+                    this.secondTop = this.maxTop ;
+                    $("#secondRollList").css("top", this.secondTop+"px");
+                    this.timer = setInterval(this.prizeListStartRoll, 1000);
+                }
+            },
+            prizeListStartRoll: function(){
+                this.firstTop = this.firstTop - 30;
+                this.secondTop = this.secondTop - 30;
+                if(-this.firstTop == this.maxTop && this.secondTop == 0){
+                    this.firstTop = this.maxTop;
+                }else if(-this.secondTop == this.maxTop && this.firstTop == 0){
+                    this.secondTop = this.maxTop;
+                }
+                $("#firstRollList").css("top", this.firstTop+"px");
+                $("#secondRollList").css("top", this.secondTop+"px");
+            },
+            startLottery: function(){
+                
             }
         }
     });
