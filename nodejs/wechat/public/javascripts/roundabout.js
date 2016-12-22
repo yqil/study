e180e67b8d5f9c8f7ff3d6402c8ea452b964f469
yql <yql@localhost.localdomain>
2016-12-22 02:51:05 -0800

    commit

--- nodejs/wechat/public/javascripts/roundabout.js
+++ nodejs/wechat/public/javascripts/roundabout.js
@@ -4,23 +4,35 @@
         data: {
             freeNum: 8,
             prizeList: [
-                {phone: "666****6666", prizeName:"四件套1"},
-                {phone: "666****6666", prizeName:"四件套2"},
-                {phone: "666****6666", prizeName:"四件套3"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"},
-                {phone: "666****6666", prizeName:"四件套床品套件"}
+                {phone: "187****6661", prizeName:"四件套床品套件"},
+                {phone: "187****6662", prizeName:"小米智能手环"},
+                {phone: "187****6663", prizeName:"吊篮藤椅"},
+                {phone: "187****6664", prizeName:"100积分奖励"},
+                {phone: "187****6665", prizeName:"500代金券"},
+                {phone: "187****6666", prizeName:"四件套床品套件"},
+                {phone: "187****6667", prizeName:"小米智能手环"},
+                {phone: "187****6668", prizeName:"500代金券"},
+                {phone: "187****6669", prizeName:"ipadmini"},
+                {phone: "187****6610", prizeName:"iphone7plus"},
+                {phone: "187****6611", prizeName:"四件套床品套件"},
+                {phone: "187****6612", prizeName:"500代金券"},
+                {phone: "187****6613", prizeName:"500代金券"},
+                {phone: "187****6614", prizeName:"500代金券"},
+                {phone: "187****6615", prizeName:"500代金券"}
                 ],
             timer: null,
+            winTimer: null,
             firstInitTop: 0,
             secondInitTop: 0,
             maxInitTop: 0,
-            dialogMsg: "服务器开小差了，请稍后尝试~"
+            dialogMsg: "服务器开小差了，请稍后尝试~",
+            currentPrizeImgNum: 0,
+            initSpeed: 200,
+            currentSpeed: 200,
+            maxSpeed: 20,
+            minSpeed: 200,
+            addSpeed: -5,
+            winNum: 0
         },
         mounted: function(){
             this.initPrizeRollList();
@@ -69,8 +81,90 @@
                 $("#secondRollList").css("top", this.secondTop+"px");
             },
             startLottery: function(){
+                if(this.freeNum > 0){
+                    this.winNum = Math.floor(Math.random()*8);
+                    console.log(this.winNum);
+                    this.currentSpeed = 200;
+                    this.addSpeed = -5;
+                    this.turntableRun();
+                    this.freeNum = this.freeNum - 1;
+                }else {
+                    this.dialogMsg = "免费次数已用完，请关注其他活动~";
+                    this.myDialog.showDialog();
+                }
+            },
+            turntableRun: function(){
+                this.clearStyle();
+                $("#prize"+this.currentPrizeImgNum).addClass("wx-roundabout-click-img");
                 
+                if(this.currentSpeed <= this.maxSpeed){
+                    this.addSpeed = -this.addSpeed;
+                }
+                this.currentSpeed += this.addSpeed; 
+                if(this.addSpeed < 0 || this.currentSpeed < this.minSpeed){
+                    if(this.currentPrizeImgNum >= 7){
+                        this.currentPrizeImgNum = 0;
+                    }else {
+                        this.currentPrizeImgNum++;
+                    }
+                    setTimeout(this.turntableRun, this.currentSpeed);
+                }else {
+                    if((this.currentPrizeImgNum) != this.winNum){
+                        if(this.currentPrizeImgNum >= 7){
+                            this.currentPrizeImgNum = 0;
+                        }else {
+                            this.currentPrizeImgNum++;
+                        }
+                        setTimeout(this.turntableRun, this.currentSpeed);
+                    }else {
+                        //
+
+                    }
+                }
+            },
+            win: function(){
+                
+            },
+            winFlash: function(obj){
+                if(obj.hasClass("wx-roundabout-click-img")){
+                    obj.removeClass("wx-roundabout-click-img")
+                }else {
+                    obj.addClass("wx-roundabout-click-img");
+                }
+            },
+            clearStyle: function(){
+                $("#prize0").removeClass("wx-roundabout-click-img");
+                $("#prize1").removeClass("wx-roundabout-click-img");
+                $("#prize2").removeClass("wx-roundabout-click-img");
+                $("#prize3").removeClass("wx-roundabout-click-img");
+                $("#prize4").removeClass("wx-roundabout-click-img");
+                $("#prize5").removeClass("wx-roundabout-click-img");
+                $("#prize6").removeClass("wx-roundabout-click-img");
+                $("#prize7").removeClass("wx-roundabout-click-img");
             }
-        }
+        },
+        components: {
+            'fail-dialog': {
+                template: '<div class="wx-roundabout-noWin">'+
+                    '<div class="wx-roundabout-noWin-mask"></div>'+
+                    '<div class="row wx-clear-margin wx-roundabout-noWin-content">'+
+                        '<div class="col-xs-12 text-center wx-clear-padding">'+
+                            '<img src="/images/wzjmmj.png" alt="图片无法显示" />'+
+                        '</div>'+
+                        '<div class="col-xs-12 text-center wx-clear-padding">'+
+                            '太可惜了，差点就中奖了~'+
+                        '</div>'+
+                        '<div class="col-xs-12 text-center wx-clear-padding" v-on:click="closeDialog">'+
+                            '我知道了'+
+                        '</div>'+
+                    '</div>'+
+                '</div>',
+                methods: {
+                    closeDialog: function(){
+                        $(".wx-roundabout-noWin").css("display", "none");
+                    }
+                }
+            }
+          }
     });
 }();
