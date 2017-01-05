09e9cdbd5defa78302dee479ff8a250704f3e004
yql <445375039@qq.com>
2017-01-05 16:46:41 +0800

    dubbo

--- java/dubbo/dubbo-example/dubbo-example-biz/src/main/java/com/mine/dubbo/example/biz/service/TestServiceImpl.java
+++ java/dubbo/dubbo-example/dubbo-example-biz/src/main/java/com/mine/dubbo/example/biz/service/TestServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mine.dubbo.example.biz.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.mine.dubbo.example.common.service.TestService;
@@ -7,9 +9,11 @@ import com.mine.dubbo.example.common.service.TestService;
 @Service("testService")
 public class TestServiceImpl implements TestService {
 
+	private static Logger log = LoggerFactory.getLogger(TestServiceImpl.class);
+	
 	@Override
 	public String test(String s) {
-		
+		log.info("传入参数：{}", s);
 		return "测试方法返回值";
 	}
 
