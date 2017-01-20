3f8d287c084124135634bda0b2f9a7e92333d4b6
yql <445375039@qq.com>
2017-01-20 19:12:48 +0800

    读写分离

---
+++ java/readwritesplit/study-example/study-example-api/src/main/java/com/mine/study/api/rwsplit/DynamicDataSourceGlobal.java
@@ -0,0 +1,5 @@
+package com.mine.study.api.rwsplit;
+
+public enum DynamicDataSourceGlobal {
+	READ, WRITE;
+}
---
+++ java/readwritesplit/study-example/study-example-api/src/main/java/com/mine/study/api/rwsplit/DynamicDataSourceHolder.java
@@ -0,0 +1,22 @@
+package com.mine.study.api.rwsplit;
+
+public class DynamicDataSourceHolder {
+
+	private static final ThreadLocal<DynamicDataSourceGlobal> holder = new ThreadLocal<DynamicDataSourceGlobal>();
+
+	private DynamicDataSourceHolder() {
+		//
+	}
+
+	public static void putDataSource(DynamicDataSourceGlobal dataSource) {
+		holder.set(dataSource);
+	}
+
+	public static DynamicDataSourceGlobal getDataSource() {
+		return holder.get();
+	}
+
+	public static void clearDataSource() {
+		holder.remove();
+	}
+}
